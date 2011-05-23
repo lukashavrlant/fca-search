49eb581305874d544ca2ae00fa72b5428d9b936e
Your Name <lukas.havrlant@gmail.com>
2011-05-23 19:59:59 +0200

    + group method for records

--- src/preprocess/database.py
+++ src/preprocess/database.py
@@ -2,6 +2,7 @@ from collections import Counter
 from common.funcfun import lmap
 from functools import reduce
 from operator import add
+from itertools import groupby
 
 
 class Database:
@@ -15,3 +16,13 @@ class Database:
 	
 	def occurences(self, counters, word):
 		return lmap(lambda x: (x[0], x[1][word]), filter(lambda x: word in x[1], counters))
+	
+	def group(self, database, keylen = 1):
+		dic = {}
+		for record in database:
+			key = record[0][0][:keylen]
+			if key in dic:
+				dic[key].append(record)
+			else:
+				dic[key] = [record]
+		return dic
