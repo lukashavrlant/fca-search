202536b5610b7cf3153658e4421b44f39ecbb1de
Your Name <lukas.havrlant@gmail.com>
2011-05-23 13:34:35 +0200

    + escape pattern

--- src/common/string.py
+++ src/common/string.py
@@ -1,6 +1,8 @@
 import re
+from common.funcfun import lmap
+
 def replace_single(text, strings, replacement):
-	return re.sub('|'.join(strings), replacement, text)	
+	return re.sub('|'.join(lmap(re.escape, strings)), replacement, text)	
 
 def replace_dict(text, dic):
 	for k, v in dic.items():
