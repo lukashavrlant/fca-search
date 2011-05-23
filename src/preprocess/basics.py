0bc508ecfadb6ac2cfddad053313898f6651b709
Your Name <lukas.havrlant@gmail.com>
2011-05-23 13:38:04 +0200

    + remove punctuation

--- src/preprocess/basics.py
+++ src/preprocess/basics.py
@@ -1,11 +1,15 @@
 from common.string import replace_single
 import re
+import string
 from common.funcfun import sreduce
 
 class Basics:
 	def compile(self, text):
-		functions =[self.remove_white_spaces]
+		functions =[self.remove_punctuation, self.remove_white_spaces]
 		return sreduce(functions, text)
 	
 	def remove_white_spaces(self, text):
 		return re.sub('\s+', ' ', text)
+	
+	def remove_punctuation(self, text):
+		return replace_single(text, list(string.punctuation), ' ')
