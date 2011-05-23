eac0f9945242cba2bd58e647ae15af5f6eeb5f7d
Your Name <lukas.havrlant@gmail.com>
2011-05-23 14:26:39 +0200

    + stemmer

--- src/preprocess/words.py
+++ src/preprocess/words.py
@@ -1,7 +1,9 @@
 from common.funcfun import sreduce
+from collections import Counter
+from common.czech_stemmer import cz_stem
 class Words:
 	def compile(self, text, stopwords):
-		funs = [self.towords, lambda x: self.remstopwords(x, stopwords)]
+		funs = [self.towords, lambda x: self.remstopwords(x, stopwords), self.tostems]
 		return sreduce(funs, text)
 	
 	def towords(self, text):
@@ -9,3 +11,9 @@ class Words:
 	
 	def remstopwords(self, words, stopwords):
 		return filter(lambda x: not(x in stopwords), words)
+	
+	def getcounter(self, words):
+		return Counter(words)
+	
+	def tostems(self, words):
+		return map(cz_stem, words)
