9006ee07a1cf74452a11d464305dbc069ecbfe9b
Lukáš Havrlant <lukas.havrlant@gmail.com>
2011-07-21 21:48:43 +0200

    Delete comment

--- src/preprocess/index_manager.py
+++ src/preprocess/index_manager.py
@@ -12,13 +12,3 @@ class IndexManager:
 			save_index(index, directory)
 		except IOError as err:
 			print("I/O error: {0}".format(err))
-		
-	
-## download files
-#urls = lmap(lambda x: 'http://localhost/fca/' + str(x) +  '.html', range(6))
-#sites = downloads(urls)
-#groups = toindex(sites)
-#save_index(groups, database_dir)
-
-#for prefix, record in groups.items():
-#	savefile(repr(record), directory + prefix + '.txt')
