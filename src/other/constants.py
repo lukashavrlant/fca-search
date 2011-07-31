f19e601a3f7bfe5fc148b6d965ff3d84b41128ae
Lukáš Havrlant <lukas.havrlant@gmail.com>
2011-07-31 19:11:25 +0200

    + stopwords file name

--- src/other/constants.py
+++ src/other/constants.py
@@ -8,7 +8,9 @@ Created on Jul 28, 2011
 ROOT = '../files/'
 INDEX_FOLDER = 'index/'
 INFO_FOLDER = 'info/'
+SRC_FILE = '../files/source/'
 
 # files
 DOCUMENT_INFO_NAME = 'documents_info.txt'
 ALL_WORDS_NAME = 'allwords.txt'
+STOPWORDS_NAME = 'stopwords.txt'
