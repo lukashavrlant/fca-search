727bcdb7304a4066b8afecb5b7e17c42b2e11964
Lukáš Havrlant <lukas.havrlant@gmail.com>
2011-08-01 16:51:44 +0200

    + functions for getting data

---
+++ src/other/data.py
@@ -0,0 +1,14 @@
+from other.constants import DATA_FOLDER, STOPWORDS_NAME
+from common.io import readfile
+
+_data = {}
+
+def stopwords():
+	return _cache_result('stopwords', lambda:  readfile(DATA_FOLDER + STOPWORDS_NAME).split())
+	
+def _cache_result(key, getValue):
+	try:
+		return _data[key]
+	except Exception:
+		_data[key] = getValue()
+		return _data[key]
