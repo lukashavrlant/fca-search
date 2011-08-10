3a2a3afd1d309291b66d95de8bd879ea404911e8
Lukáš Havrlant <lukas.havrlant@gmail.com>
2011-08-10 20:08:30 +0200

    refactoring

--- src/search.py
+++ src/search.py
@@ -21,21 +21,21 @@ def printSearch(query, nosco):
 	result = list(nosco.search(query))
 	
 	if result:
-		maxURL = max(len(x['url']) for x in result) - len('http://localhost/matweb/') + len('matweb.cz/')
+		maxURL = max(len(x['url']) for x in result)
+		if 'matweb' in result[0]['url']:
+			maxURL = maxURL - len('http://localhost/matweb/') + len('matweb.cz/')
 		maxKeywords = max([len(repr(x['keywords'][:3])) for x in result])
 		maxPos = len(str(len(result)))
 		maxScore = max(len(str(round(x['score'], 3))) for x in result) + 1
 		
 		print(''.ljust(maxPos), 'URL'.ljust(maxURL), 'KEYWORDS'.ljust(maxKeywords), 'SCORE'.ljust(maxScore), 'WORDSCOUNT')
-		i = 1
-		for item in result:
+		for pos, item in enumerate(result):
 			score = str(round(item['score'], 3))
 			url = item['url'].replace('http://localhost/matweb/', 'matweb.cz/')
 			keywords = repr(item['keywords'][:3])
 			wordscount = item['wordscount']
 	
-			print(str(i).ljust(maxPos), url.ljust(maxURL), keywords.ljust(maxKeywords), score.ljust(maxScore), wordscount)
-			i += 1
+			print(str(pos+1).ljust(maxPos), url.ljust(maxURL), keywords.ljust(maxKeywords), score.ljust(maxScore), wordscount)
 	
 	print()
 	print('Search query: ' + str(nosco.lastQuery))
@@ -56,7 +56,7 @@ nosco = None
 index = IndexManager()
 
 
-print('Welcome in FCA search!')
+print('Welcome to FCA search!')
 
 while(True):
 	cmdstring = input('> ').strip()
@@ -65,8 +65,13 @@ while(True):
 	if cmd == 'exit':
 		print('Good bye!')
 		break
-	elif cmd == 'q':
-		query = getQuery(cmdstring)
+	elif cmd == 'bi':
+		urlsFilename = getQuery(cmdstring)
+		urls = eval(readfile(DATA_FOLDER + urlsFilename + '.txt'))
+		index.build(urls, database, getStopWords())
+		print('Done.')
+	else:
+		query = cmdstring
 		if nosco:
 			printSearch(query, nosco)
 		else:
@@ -75,10 +80,3 @@ while(True):
 				printSearch(query, nosco)
 			except Exception as e:
 				print(str(e))
-	elif cmd == 'bi':
-		urlsFilename = getQuery(cmdstring)
-		urls = eval(readfile(DATA_FOLDER + urlsFilename + '.txt'))
-		index.build(urls, database, getStopWords())
-		print('Done.')
-	else:
-		print('Unknown command')
