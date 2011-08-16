8f12a46c5bfb953f535b3cee1598151cc1b2813e
Lukáš Havrlant <lukas.havrlant@gmail.com>
2011-08-16 11:51:14 +0200

    + context2slf

--- src/fca_extension/utilities.py
+++ src/fca_extension/utilities.py
@@ -2,6 +2,8 @@ from functools import reduce
 from operator import add
 from fca.context import Context
 
+
+## Search result -> context
 def getContextFromSR(searchResult, index):
 	keywords = _selectColumn('keywords', searchResult)
 	keywords = list(set(reduce(add, keywords)))
@@ -25,3 +27,28 @@ def _getTable(index, ids, keywords):
 
 def _selectColumn(name, table):
 	return [x[name] for x in table]
+
+## Context -> slf
+def context2slf(context):
+	text = ['[Lattice]']
+	text.append(str(context.height))
+	text.append(str(context.width))
+	text.append('[Objects]')
+	
+	for obj in context.objects:
+		text.append(obj)
+		
+	text.append('[Attributes]')
+	
+	for attr in context.attributes:
+		text.append(attr)
+		
+	text.append('[relation]')
+	
+	for line in context.table:
+		string = ''
+		for value in line:
+			string += '1 ' if value else '0 '
+		text.append(string)
+		
+	return '\n'.join(text)
