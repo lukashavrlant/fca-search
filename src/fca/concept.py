da3a5ae4fa1379e3456ef6150866498d18b3baf7
Lukáš Havrlant <lukas.havrlant@gmail.com>
2011-08-16 11:29:12 +0200

    concept accessors

--- src/fca/concept.py
+++ src/fca/concept.py
@@ -1,7 +1,13 @@
 class Concept():
-	def __init__(self, extent, intent):
-		self.extent = extent
-		self.intent = intent
+	def __init__(self, extent = set(), intent = set()):
+		self.setExtent(extent)
+		self.setIntent(intent)
+		
+	def setExtent(self, extent):
+		self.extent = set(extent)
+	
+	def setIntent(self, intent):
+		self.intent = set(intent)
 
 	def __repr__(self):
 		return "(" + repr(self.extent) + ", " + repr(self.intent) + ")"
