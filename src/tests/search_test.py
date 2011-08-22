a1e1f80190946b6a8cbd617b22778e73004f08a1
Lukáš Havrlant <lukas.havrlant@gmail.com>
2011-08-22 21:53:10 +0200

    test updated

--- src/tests/search_test.py
+++ src/tests/search_test.py
@@ -8,19 +8,6 @@ from subprocess import Popen, PIPE
 
 
 class SearchTest(unittest.TestCase):
-
-	def test_SearchEngine(self):
-		temp = SearchEngine(Index(TEST_FOLDER + 'database/'), getStopWords()).search
-		fun = lambda query: repr(temp(query)['documents'])
-		ass = self.assertEquals
-		read = lambda name: readfile(TEST_FOLDER + 'results/' + name + '.txt')
-		
-		ass(fun('nesmysl'), read('search1'))
-		ass(fun('derivace'), read('search2'))
-		ass(fun('rovnice'), read('search3'))
-		ass(fun('nonsense'), read('search4'))
-		ass(fun('derivace+mnoziny'), read('search5'))
-		ass(fun('byvaji'), read('search6'))
 		
 	def testSearchCommand(self):
 		path = TEST_FOLDER + 'results/'
@@ -31,11 +18,16 @@ class SearchTest(unittest.TestCase):
 		read = lambda name: readfile(path + name + '.txt')
 		ass = self.assertEquals
 		
-		ass(dfun('matweb', 'derivace'), read('derivace'))
-		ass(dfun('matweb', 'nesmysl'), read('nesmysl'))
-		ass(dfun('matweb', '(spocetne OR nespocetne) mnoziny'), read('spocetne'))
-		ass(dfun('jpw', 'input NOT select'), read('input'))
-		ass(dfun('inf', 'belohlavek'), read('belohlavek'))
+		
+		dtbs = ['matweb', 'matweb', 'matweb', 'jpw']
+		queries = ['derivace', 'nesmysl', '(spocetne OR nespocetne) mnoziny', 'input NOT select']
+		filenames = ['derivace', 'nesmysl', 'spocetne', 'input']
+		
+		for d, q, f in zip(dtbs, queries, filenames):
+			#save(fun(d, q), f)
+			ass(dfun(d, q), read(f))
+		
+		
 
 
 if __name__ == "__main__":
