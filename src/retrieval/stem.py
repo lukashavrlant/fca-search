21ef0a4b90ae46519db911e126f6b6fd7a762bc0
Lukáš Havrlant <lukas.havrlant@gmail.com>
2011-08-22 22:06:41 +0200

    refatoring

--- src/retrieval/stem.py
+++ src/retrieval/stem.py
@@ -1,18 +1,14 @@
 class Stem:
-	def __init__(self, line = False):
-		if line:
-			self.stem = line
-			self.name = line[0][0]
-			self.totalcount = line[0][1]
-			self.documents = dict(line[1])
+	def __init__(self, record = None, stem = None):
+		if record:
+			self.name = stem
+			self.documents = dict(record)
 			self.docids = self.documents.keys()
 		else:
-			self.stem = ''
 			self.name = ''
-			self.totalcount = 0
 			self.documents = {}
 			self.docids = [] 
-		
+			
 		
 	def __repr__(self):
-		return repr({'stem':self.name, 'count':self.totalcount, 'documents':self.documents, 'docids':self.docids})
+		return repr({'stem':self.name, 'documents':self.documents, 'docids':self.docids})
