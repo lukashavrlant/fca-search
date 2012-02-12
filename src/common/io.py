a8179769bbf13e5ec69e0439650b81748c8cb33a
Lukáš Havrlant <lukas.havrlant@gmail.com>
2012-02-13 00:22:32 +0100

    + md5 hash of file

--- src/common/io.py
+++ src/common/io.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen, urlretrieve
 from common.funcfun import each, lmap
 import codecs
+import hashlib
 
 def download(site):
 	return urlopen(site).read().decode('utf-8')
@@ -36,3 +37,14 @@ def trySaveFile(content, path, charset = 'utf-8'):
 	
 def downloadFile(url, destination):
 	urlretrieve(url, destination)
+
+def md5(fileName):
+    md5 = hashlib.md5()
+    fd = open(fileName,"rb")
+    while True:
+        buffer = fd.read(1024)
+        md5.update(buffer)
+        if len(buffer) < 1024:
+            break
+    fd.close()
+    return md5.hexdigest()
