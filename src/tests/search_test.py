7c7e1ec11288449b0e7fc5528504c1bb3f744245
Lukáš Havrlant <lukas.havrlant@gmail.com>
2012-02-17 00:45:39 +0100

    for is not a good idea…

--- src/tests/search_test.py
+++ src/tests/search_test.py
@@ -21,10 +21,13 @@ class SearchTest(unittest.TestCase):
 		# search matweb
 		queries = ['derivace', 'nesmysl', '(spocetne OR nespocetne) mnoziny', 'rovnice', 'rovnice NOT (linearni OR pravdepodobnost)']
 
-		for num, q in enumerate(queries):
-			filename = 'matweb' + str(num)
-			# save(fun('matweb', q), filename)
-			ass(dfun('matweb', q), read(filename))
+		ass(dfun('matweb', 'derivace'), read('matweb0'))
+		ass(dfun('matweb', 'nesmysl'), read('matweb1'))
+
+		# for num, q in enumerate(queries):
+		# 	filename = 'matweb' + str(num)
+		#  	save(fun('matweb', q), filename)
+
 
 
 	def test_spellcheck(self):
