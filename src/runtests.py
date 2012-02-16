3802cb627f21adeaf273da2113454ef5a16b3633
Lukáš Havrlant <lukas.havrlant@gmail.com>
2012-02-17 00:34:55 +0100

    tests updated

--- src/runtests.py
+++ src/runtests.py
@@ -1,7 +1,7 @@
 from tests.common_tests import *
 from tests.boolean_parser_tests import *
 from tests.index_tests import TestIndex
-# from tests.preprocess_test import *
+from tests.preprocess_test import *
 # from tests.search_test import *
 from unittest import main
 
--- src/tests/preprocess_test.py
+++ src/tests/preprocess_test.py
@@ -3,8 +3,9 @@ from preprocess.words import towords, remstopwords
 from common.io import readfiles, readfile, savefile
 from common.funcfun import lmap
 from preprocess.index_builder import toIndex
-from other.constants import TEST_FOLDER
+from other.constants import TEST_FOLDER, DATA_FOLDER
 from other.data import getStopWords
+from common.io import downloads
 
 class TestWords(unittest.TestCase):
 	def test_towords(self):
@@ -18,25 +19,30 @@ class TestWords(unittest.TestCase):
 		self.assertEqual(['a', 'b'], list(remstopwords(['a', 'b'], ['c'])))
 		
 	def test_toIndex(self):
-		urls = ['binomicka-veta.html', 'prirozena-cisla.html', 'pythagorova-veta.html', 'rovnice.html', 'zaklady-statistiky.html']
-		sites = readfiles(lmap(lambda x: TEST_FOLDER + 'sites/' + x, urls))
+		urls = self.getURLs()
+		sites = downloads(urls)
 		sites = [{'type':'html', 'content':x, 'url':'url'} for x in sites]
 		
-		# savefile(repr(toIndex(sites, [], 1)), TEST_FOLDER + 'results/index1.txt')
-		# savefile(repr(toIndex(sites, getStopWords(), 1)), TEST_FOLDER + 'results/index2.txt')
-		# savefile(repr(toIndex(sites, getStopWords(), 2)), TEST_FOLDER + 'results/index3.txt')
+		# savefile(repr(toIndex(sites, [], 1)), TEST_FOLDER + 'index1.txt')
+		# savefile(repr(toIndex(sites, getStopWords(), 1)), TEST_FOLDER + 'index2.txt')
+		# savefile(repr(toIndex(sites, getStopWords(), 2)), TEST_FOLDER + 'index3.txt')
 		
 		result = toIndex(sites, [], 1)
-		desired = readfile(TEST_FOLDER + 'results/index1.txt')
+		desired = readfile(TEST_FOLDER + 'index1.txt')
 		self.assertEqual(repr(result), desired)
 		
 		result = toIndex(sites, getStopWords(), 1)
-		desired = readfile(TEST_FOLDER + 'results/index2.txt')
+		desired = readfile(TEST_FOLDER + 'index2.txt')
 		self.assertEqual(repr(result), desired)
 		
 		result = toIndex(sites, getStopWords(), 2)
-		desired = readfile(TEST_FOLDER + 'results/index3.txt')
+		desired = readfile(TEST_FOLDER + 'index3.txt')
 		self.assertEqual(repr(result), desired)
+
+	def getURLs(cls):
+		path = DATA_FOLDER + 'test.txt'
+		content = readfile(path).splitlines()
+		return content
 		
 		
 		
