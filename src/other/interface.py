1bce1ae64ff89a1dd1a02a75be72fe2add89e357
Lukáš Havrlant <lukas.havrlant@gmail.com>
2012-03-06 10:16:01 +0100

    buildIndex function moved

--- src/other/interface.py
+++ src/other/interface.py
@@ -1,10 +1,12 @@
+from preprocess.index_manager import IndexManager
 from other.constants import SETTINGS_FILE
-from other.constants import DATABASES_FOLDER
+from other.constants import DATABASES_FOLDER, DATA_FOLDER
 from other.settings import Settings
 from retrieval.search_engine import SearchEngine
 from retrieval.index import Index
 from fca_extension.fca_search_engine import FCASearchEngine
 from other.data import getStopWords
+from common.io import readfile
 
 def searchQuery(databaseName, query, stopwatch = None):
 	database = DATABASES_FOLDER + databaseName + '/'
@@ -14,3 +16,15 @@ def searchQuery(databaseName, query, stopwatch = None):
 	fca = FCASearchEngine(searchEngine, index, settings)
 	searchResults = fca.search(query)
 	return searchResults
+
+
+def buildIndex(databaseName, linksSourcePath, currSettings):
+	settings = Settings(DATA_FOLDER + SETTINGS_FILE)
+	for key, value in currSettings.items():
+		settings.set(key, value)
+
+	database = DATABASES_FOLDER + databaseName + '/'
+	links = readfile(linksSourcePath).splitlines()
+	indexManager = IndexManager(settings)
+	indexManager.shutUp = False
+	indexManager.build(links, database, getStopWords())
