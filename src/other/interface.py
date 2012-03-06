e48e27f3c92ae12d1b600bd1c6e25318e9207d56
Lukáš Havrlant <lukas.havrlant@gmail.com>
2012-03-06 13:14:08 +0100

    + options

--- src/other/interface.py
+++ src/other/interface.py
@@ -7,11 +7,10 @@ from retrieval.index import Index
 from fca_extension.fca_search_engine import FCASearchEngine
 from other.data import getStopWords
 from common.io import readfile
+from common.czech_stemmer import createStem
 
 def searchQuery(databaseName, query, stopwatch = None):
-	database = DATABASES_FOLDER + databaseName + '/'
-	settings = Settings(database + SETTINGS_FILE)
-	index = Index(database, settings)
+	index, settings = getIndexAndSettings(databaseName)
 	searchEngine = SearchEngine(index, getStopWords())
 	fca = FCASearchEngine(searchEngine, index, settings)
 	searchResults = fca.search(query)
@@ -28,3 +27,40 @@ def buildIndex(databaseName, linksSourcePath, currSettings):
 	indexManager = IndexManager(settings)
 	indexManager.shutUp = False
 	indexManager.build(links, database, getStopWords())
+
+def findURL(databaseName, match):
+	links = getAllLinks(databaseName)
+	return [x for x in links if match in x]
+
+def findDocID(databaseName, URLmatch):
+	links = []
+	for i, url in enumerate(getAllLinks(databaseName)):
+		if URLmatch in url:
+			links.append((url, i))
+	return dict(links)
+
+def documentFrequency(databaseName, word):
+	return getIndex(databaseName).document_frequency(createStem(word))
+
+	
+def wordInDocFrequency(databaseName, word, docID):
+	return getIndex(databaseName).term_frequency(createStem(word), int(docID))
+
+def wordFrequency(databaseName, word):
+	return getIndex(databaseName).totalTermFrequency(createStem(word))
+
+def getAllWords(databaseName):
+	return getIndex(databaseName).getAllWords()
+
+def getAllLinks(databaseName):
+	index = getIndex(databaseName)
+	return index.getLinks()
+
+def getIndex(databaseName):
+	return getIndexAndSettings(databaseName)[0]
+
+def getIndexAndSettings(databaseName):
+	database = DATABASES_FOLDER + databaseName + '/'
+	settings = Settings(database + SETTINGS_FILE)
+	index = Index(database, settings)
+	return index, settings
